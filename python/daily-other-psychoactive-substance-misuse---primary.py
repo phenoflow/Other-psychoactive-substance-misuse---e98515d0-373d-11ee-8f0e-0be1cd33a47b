581b378269c70bc56d2b23dc7adc3748928ceb45
Martin Chapman <contact@martinchapman.co.uk>
2023-08-10 06:28:00 +0100

    Update made by Phenoflow

---
+++ python/daily-other-psychoactive-substance-misuse---primary.py
@@ -0,0 +1,25 @@
+# Kuan V, Denaxas S, Gonzalez-Izquierdo A, Direk K, Bhatti O, Husain S, Sutaria S, Hingorani M, Nitsch D, Parisinos C, Lumbers T, Mathur R, Sofat R, Casas JP, Wong I, Hemingway H, Hingorani A, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"1T10.00","system":"readv2"},{"code":"1T40.00","system":"readv2"},{"code":"1T50.00","system":"readv2"},{"code":"1T80.00","system":"readv2"},{"code":"1T90.00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('other-psychoactive-substance-misuse-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["daily-other-psychoactive-substance-misuse---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["daily-other-psychoactive-substance-misuse---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["daily-other-psychoactive-substance-misuse---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
